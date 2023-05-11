ea54401350b683338da0f393bb0f46717089b8a3
oboyle3 <po720999@sju.edu>
2023-05-11 14:40:36 -0400

    new

--- lib/member_Pages/rulesPageMember.dart
+++ lib/member_Pages/rulesPageMember.dart
@@ -25,24 +25,3 @@ class RulesPageMember extends StatelessWidget {
   );
 }
 
-/*
-class rulesPageCaddy extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar:AppBar(  
-      title: Text('caddy rules'),
-      centerTitle:true,
-      backgroundColor: Colors.green,
-    ),
-    body: Column(
-        children: [
-          SizedBox(height: 25), // add some space between app bar and text
-          Text(
-            'Review the rules at your club',
-            style: TextStyle(fontSize: 25),
-          ),
-        ],
-      ),
-  );
-}
-*/
