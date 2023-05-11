0fcbd8a23c7889ff6745a2c2f3e28bd462681301
oboyle3 <po720999@sju.edu>
2023-05-11 09:36:12 -0400

    change

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home:  NewMainPage(), //was const  3/7/2023 home: SignInScreen(),
+      home:  NewMainPage(), 
     );
   }
 }
